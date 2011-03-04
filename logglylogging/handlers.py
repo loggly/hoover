be20a881c897a70f00b1b8548f3c6c146abc7916
Mike Blume <mike@loggly.com>
2011-03-04 02:37:14 +0000

    actually set object attributes. Also: good intentions!

--- logglylogging/handlers.py
+++ logglylogging/handlers.py
@@ -6,7 +6,7 @@ class LogglyHandler(logging.Handler):
     pass
 
 class LogglyHttpHandler(LogglyHandler):
-    def __init__ (token='', inputname='', input=None):
+    def __init__ (self, token='', inputname='', input=None, announce=False):
         if inputname:
             try:
                 (input,) = [i for i in html_inputs() if i['name'] == inputname]
@@ -14,11 +14,19 @@ class LogglyHttpHandler(LogglyHandler):
                 # TODO: create/raise appropriate exception
                 raise
         if input:
+            self.inputobj = input
             try:
                 token = input['input_token']
+                self.inputname = input['name']
             except:
                 #TODO
                 raise
+        self.token = token
+        # TODO: verify we can write to the input
+        if announce:
+            # TODO: grab this boxes' IP, and announce logging to the input
+            pass
+            
 
 
 class LogglySyslogHandler(LogglyHandler):
