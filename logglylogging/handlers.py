1fcf91ce8f9e8c3bf68832755226e3292d16b264
Mike Blume <mike@loggly.com>
2011-03-04 02:28:50 +0000

    tell an HTTP handler how to find its token

--- logglylogging/handlers.py
+++ logglylogging/handlers.py
@@ -1,10 +1,25 @@
 import logging
 
+from logglylogging.utils import html_inputs
+
 class LogglyHandler(logging.Handler):
     pass
 
 class LogglyHttpHandler(LogglyHandler):
-    pass
+    def __init__ (token='', inputname='', input=None):
+        if inputname:
+            try:
+                (input,) = [i for i in html_inputs() if i['name'] == inputname]
+            except:
+                # TODO: create/raise appropriate exception
+                raise
+        if input:
+            try:
+                token = input['input_token']
+            except:
+                #TODO
+                raise
+
 
 class LogglySyslogHandler(LogglyHandler):
     pass
