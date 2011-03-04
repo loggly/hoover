cd8be6585e2a86c9e2ae84aaab4fd39d0f5c2989
Mike Blume <mike@loggly.com>
2011-03-03 23:01:20 -0800

    check in super simple setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='Loggly Logger',
+      version='0.1',
+      description="Library for logging to Loggly from within Python webapps",
+      author="Mike Blume",
+      author_email="mike@loggly.com",
+      # fix this
+      url="http://www.loggly.com",
+      packages=['logglylogging']
+)
