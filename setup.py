7698d9cb0b60861560b6476745f2a945435a8872
Mike Blume <mike@loggly.com>
2011-06-15 19:31:52 +0000

    use the github url

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ setup(name='Hoover',
       description="Library for logging to Loggly from within Python webapps",
       author="Mike Blume",
       author_email="mike@loggly.com",
-      # fix this
-      url="http://www.loggly.com",
+      url="http://www.github.com/loggly/hoover",
       packages=['hoover'],
       install_requires=['httplib2'],
 )
